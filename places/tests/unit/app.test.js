465374371b7079ae6339f1fe9a979026647c61e5
Norbert Logiewa <norbertlogiewa96@gmail.com>
2020-04-13 16:18:47 +0100

    Test teaks

--- places/tests/unit/app.test.js
+++ places/tests/unit/app.test.js
@@ -3,20 +3,17 @@
 const app = require('../../app.js');
 const chai = require('chai');
 const expect = chai.expect;
-var event, context;
+const context = {};
+const event = require('../../event.json');
 
-describe('Tests index', function () {
+describe('test places lambda endpoint', function () {
     it('verifies successful response', async () => {
         const result = await app.handler(event, context)
-
         expect(result).to.be.an('object');
         expect(result.statusCode).to.equal(200);
         expect(result.body).to.be.an('string');
 
-        let response = JSON.parse(result.body);
-
-        expect(response).to.be.an('object');
-        expect(response.message).to.be.equal("hello world");
-        // expect(response.location).to.be.an("string");
+        const response = JSON.parse(result.body);
+        expect(response).to.be.an('array');
     });
 });
